486d0ec91ffc8fabbb2d0a0d5106141f7b473fe6
Hiteshwar Vadlamudi <hiteshv@Hiteshs-MacBook-Pro.local>
2016-03-28 12:51:20 -0400

    Initial Commit

---
+++ MogeanSDK/MogeanSDK.framework/Headers/MogeanSDK.h
@@ -0,0 +1,19 @@
+//
+//  MogeanSDK.h
+//  MogeanSDK
+//
+//  Created by Hiteshwar Vadlamudi
+//  Copyright © 2015 Mogean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MogeanSDK.
+FOUNDATION_EXPORT double MogeanSDKVersionNumber;
+
+//! Project version string for MogeanSDK.
+FOUNDATION_EXPORT const unsigned char MogeanSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MogeanSDK/PublicHeader.h>
+
+
